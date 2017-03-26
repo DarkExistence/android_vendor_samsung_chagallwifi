f802cffc8d0d98bba59f334d4c96957649176f19
deadman96385 <seanhoyt963@gmail.com>
2017-03-26 14:41:18 +0100

    Cleanup

--- chagallwifi-vendor-blobs.mk
+++ chagallwifi-vendor-blobs.mk
@@ -12,11 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#RIL
-#libGLES_trace.so needed as else protobuf error
-PRODUCT_COPY_FILES += \
-    vendor/samsung/chagallwifi/proprietary/sbin/cbd:root/sbin/cbd
-
 #Bluetooth firmware
 PRODUCT_COPY_FILES += \
     vendor/samsung/chagallwifi/proprietary/vendor/firmware/bcm4350_V0301.0601.hcd:system/vendor/firmware/bcm4350_V0301.0601.hcd
@@ -29,8 +24,6 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/chagallwifi/proprietary/etc/wifi/nvram_net.txt_4354_a1:system/etc/wifi/nvram_net.txt_4354_a1 \
 
 #Sensors
-#Binary file DBT-T800XXU1BOJ1-20151023095433/system/lib/hw/sensors.universal5420.so matches
-#Binary file DBT-T800XXU1BOJ1-20151023095433/system/lib/libak09911c.so matches
 PRODUCT_COPY_FILES += \
     vendor/samsung/chagallwifi/proprietary/lib/hw/sensors.universal5420.so:system/lib/hw/sensors.universal5420.so
 
@@ -39,15 +32,8 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/chagallwifi/proprietary/bin/gpsd:system/bin/gpsd \
     vendor/samsung/chagallwifi/proprietary/lib/hw/gps.default.so:system/lib/hw/gps.default.so
 
-#GPS - Config TODO: Move to device
-PRODUCT_COPY_FILES += \
-    vendor/samsung/chagallwifi/proprietary/etc/gps.xml:system/etc/gps.xml
-
 #SSWAP - Needed?
 PRODUCT_COPY_FILES += \
-    vendor/samsung/chagallwifi/proprietary/sbin/sswap:root/sbin/sswap
+    vendor/samsung/klimtwifi/proprietary/bin/sswap:root/sbin/sswap
 
-#MediaDRM
-PRODUCT_COPY_FILES_NN += \
-    vendor/samsung/chagallwifi/proprietary/vendor/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so
 
